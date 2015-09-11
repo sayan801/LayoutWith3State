dbd07fe0b4df5c423a7b6a6ed243204baa00509c
amitabha-mycuratio <amitabha.biswas@mycuratio.com>
2015-09-11 14:02:04 +0530

    renamed and organize and define code

--- SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
+++ SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
@@ -1,17 +1,12 @@
 package app.com.simplesliderdemo;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 
@@ -19,23 +14,23 @@ import java.util.ArrayList;
 
 public class SimpleSliderActivity extends FragmentActivity
 {
-
-//    TextView middle;
-    LinearLayout top, bottom, middle;
+    LinearLayout LL_top, LL_bottom, LL_middle;
     int DeviceTotalWidth, DeviceTotalHeight;
-    int i = 33; // Initial value shoud be 33 for Default State
+    int step = 33; // Initial value shoud be 33 for Default State
+    int noOfsteate = 99; // used 99 for '3' step, usded 66 for '2' step
 
     //for Google map
-    GoogleMap map;
+    GoogleMap gMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_slider);
 
-        top = (LinearLayout) findViewById(R.id.LL_top);
-        middle = (LinearLayout) findViewById(R.id.LL_middle);
-        bottom = (LinearLayout) findViewById(R.id.LL_bottom);
+        LL_top = (LinearLayout) findViewById(R.id.LL_top);
+        LL_middle = (LinearLayout) findViewById(R.id.LL_middle);
+        LL_bottom = (LinearLayout) findViewById(R.id.LL_bottom);
 
         DisplayMetrics metrics = getResources().getDisplayMetrics();
 
@@ -44,52 +39,52 @@ public class SimpleSliderActivity extends FragmentActivity
         //Log.d("DeviceTotalHeight <> ", DeviceTotalHeight+" <> " +DeviceTotalHeight*10/100 + " <> "+DeviceTotalHeight*45/100);
 
         //During Launch  set default value
-        bottom.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
-        middle.getLayoutParams().height= (int) (DeviceTotalHeight*8/100);
-        top.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
+        LL_bottom.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
+        LL_middle.getLayoutParams().height= (int) (DeviceTotalHeight*8/100);
+        LL_top.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
 
-         middle.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                // onClick change the State..by increasing value programetically
-                i = i+33;
+         LL_middle.setOnClickListener(new View.OnClickListener()
+         {
+             @Override
+             public void onClick(View view)
+             {
+                 // onClick change the State..by increasing value programetically
+                 step = step + 33;
 
-                if(i == 33)
-                {
-                    //for Default view
-                    top.setVisibility(View.VISIBLE);
-                    bottom.setVisibility(View.VISIBLE);
+                 if (step == 33)
+                 {
+                     //for Default view
+                     LL_top.setVisibility(View.VISIBLE);
+                     LL_bottom.setVisibility(View.VISIBLE);
 
-                    bottom.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
-                    middle.getLayoutParams().height= (int) (DeviceTotalHeight*8/100);
-                    top.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
-                }
-                else if(i == 66)
-                {
-                    //slider Top positon
-                    top.setVisibility(View.GONE);
-                    bottom.setVisibility(View.VISIBLE);
+                     LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 45 / 100);
+                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 8 / 100);
+                     LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 45 / 100);
+                 }
+                 else if (step == 66)
+                 {
+                     //slider Top positon
+                     LL_top.setVisibility(View.GONE);
+                     LL_bottom.setVisibility(View.VISIBLE);
 
-                    bottom.getLayoutParams().height= (int) (DeviceTotalHeight*90/100);
-                    middle.getLayoutParams().height= (int) (DeviceTotalHeight*10/100);
-                }
-                else
-                {
-                    //slider should be Buttom positon
-                    top.setVisibility(View.VISIBLE);
-                    bottom.setVisibility(View.GONE);
+                     LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 90 / 100);
+                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10 / 100);
+                 }
+                 else
+                 {
+                     //slider should be Buttom positon
+                     LL_top.setVisibility(View.VISIBLE);
+                     LL_bottom.setVisibility(View.GONE);
 
-                    top.getLayoutParams().height= (int) (DeviceTotalHeight*90/100);
-                    middle.getLayoutParams().height= (int) (DeviceTotalHeight*10/100);
-                }
+                     LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 90 / 100);
+                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10 / 100);
+                 }
 
-                //continue this Loop
-                if(i == 99)
-                    i=0;
-            }
-        });
+                 //continue this Loop
+                 if (step == noOfsteate)
+                     step = 0; // moved to initial State
+             }
+         });
 
         ArrayList<String> stringArray = new ArrayList<String>();
 
