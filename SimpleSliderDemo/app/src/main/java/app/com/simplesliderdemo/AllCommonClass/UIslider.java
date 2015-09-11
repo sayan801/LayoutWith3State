624b77f93ad8832ddc68d5a3e1a08a93b87b9137
amitabha-mycuratio <amitabha.biswas@mycuratio.com>
2015-09-11 14:15:40 +0530

    used common classs moved code to common class

--- SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/AllCommonClass/UIslider.java
+++ SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/AllCommonClass/UIslider.java
@@ -3,8 +3,15 @@ package app.com.simplesliderdemo.AllCommonClass;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import app.com.simplesliderdemo.R;
 
@@ -13,6 +20,11 @@ import app.com.simplesliderdemo.R;
  */
 public class UIslider extends LinearLayout
 {
+    LinearLayout LL_top, LL_bottom, LL_middle;
+    int DeviceTotalWidth, DeviceTotalHeight;
+    int step = 33; // Initial value shoud be 33 for Default State
+    int noOfsteate = 99; // used 99 for '3' step, usded 66 for '2' step
+
     public UIslider(Context context, AttributeSet attrs)
     {
         super(context, attrs);
@@ -22,6 +34,64 @@ public class UIslider extends LinearLayout
     protected void onFinishInflate()
     {
         super.onFinishInflate();
+
+        LL_top = (LinearLayout) findViewById(R.id.LL_top);
+        LL_middle = (LinearLayout) findViewById(R.id.LL_middle);
+        LL_bottom = (LinearLayout) findViewById(R.id.LL_bottom);
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        DeviceTotalWidth = metrics.widthPixels;
+        DeviceTotalHeight = metrics.heightPixels;
+        //Log.d("DeviceTotalHeight <> ", DeviceTotalHeight+" <> " +DeviceTotalHeight*10/100 + " <> "+DeviceTotalHeight*45/100);
+
+        //During Launch  set default value
+        LL_bottom.getLayoutParams().height= (int) (DeviceTotalHeight * 45/100);
+        LL_middle.getLayoutParams().height= (int) (DeviceTotalHeight * 8/100);
+        LL_top.getLayoutParams().height= (int) (DeviceTotalHeight * 45/100);
+
+        LL_middle.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                // onClick change the State..by increasing value programetically
+                step = step + 33;
+
+                if (step == 33)
+                {
+                    //for Default view
+                    LL_top.setVisibility(View.VISIBLE);
+                    LL_bottom.setVisibility(View.VISIBLE);
+
+                    LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 45/100);
+                    LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 8/100);
+                    LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 45/100);
+                }
+                else if (step == 66)
+                {
+                    //slider Top positon
+                    LL_top.setVisibility(View.GONE);
+                    LL_bottom.setVisibility(View.VISIBLE);
+
+                    LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 90/100);
+                    LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10/100);
+                }
+                else
+                {
+                    //slider should be Buttom positon
+                    LL_top.setVisibility(View.VISIBLE);
+                    LL_bottom.setVisibility(View.GONE);
+
+                    LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 90/100);
+                    LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10/100);
+                }
+
+                //continue this Loop
+                if (step == noOfsteate)
+                    step = 0; //moved to initial State
+            }
+        });
 
     }
     public void init(Context context)
--- SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
+++ SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
@@ -14,82 +14,17 @@ import java.util.ArrayList;
 
 public class SimpleSliderActivity extends FragmentActivity
 {
-    LinearLayout LL_top, LL_bottom, LL_middle;
-    int DeviceTotalWidth, DeviceTotalHeight;
-    int step = 33; // Initial value shoud be 33 for Default State
-    int noOfsteate = 99; // used 99 for '3' step, usded 66 for '2' step
-
-    //for Google map
-    GoogleMap gMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_slider);
 
-        LL_top = (LinearLayout) findViewById(R.id.LL_top);
-        LL_middle = (LinearLayout) findViewById(R.id.LL_middle);
-        LL_bottom = (LinearLayout) findViewById(R.id.LL_bottom);
-
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-
-         DeviceTotalWidth = metrics.widthPixels;
-         DeviceTotalHeight = metrics.heightPixels;
-        //Log.d("DeviceTotalHeight <> ", DeviceTotalHeight+" <> " +DeviceTotalHeight*10/100 + " <> "+DeviceTotalHeight*45/100);
-
-        //During Launch  set default value
-        LL_bottom.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
-        LL_middle.getLayoutParams().height= (int) (DeviceTotalHeight*8/100);
-        LL_top.getLayoutParams().height= (int) (DeviceTotalHeight*45/100);
-
-         LL_middle.setOnClickListener(new View.OnClickListener()
-         {
-             @Override
-             public void onClick(View view)
-             {
-                 // onClick change the State..by increasing value programetically
-                 step = step + 33;
-
-                 if (step == 33)
-                 {
-                     //for Default view
-                     LL_top.setVisibility(View.VISIBLE);
-                     LL_bottom.setVisibility(View.VISIBLE);
-
-                     LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 45 / 100);
-                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 8 / 100);
-                     LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 45 / 100);
-                 }
-                 else if (step == 66)
-                 {
-                     //slider Top positon
-                     LL_top.setVisibility(View.GONE);
-                     LL_bottom.setVisibility(View.VISIBLE);
-
-                     LL_bottom.getLayoutParams().height = (int) (DeviceTotalHeight * 90 / 100);
-                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10 / 100);
-                 }
-                 else
-                 {
-                     //slider should be Buttom positon
-                     LL_top.setVisibility(View.VISIBLE);
-                     LL_bottom.setVisibility(View.GONE);
-
-                     LL_top.getLayoutParams().height = (int) (DeviceTotalHeight * 90 / 100);
-                     LL_middle.getLayoutParams().height = (int) (DeviceTotalHeight * 10 / 100);
-                 }
-
-                 //continue this Loop
-                 if (step == noOfsteate)
-                     step = 0; // moved to initial State
-             }
-         });
-
         ArrayList<String> stringArray = new ArrayList<String>();
 
+        // ListView Item
         for (int i=0; i<50 ; i++)
-        stringArray.add(i + " no Item");
+            stringArray.add(i + " no Item");
         //Listview Code
         ArrayAdapter modeAdapter = new ArrayAdapter(this, R.layout.list_item_adapter, R.id.provider, stringArray);
         ListView LV_bottom = (ListView) findViewById(R.id.LV_bottom);
