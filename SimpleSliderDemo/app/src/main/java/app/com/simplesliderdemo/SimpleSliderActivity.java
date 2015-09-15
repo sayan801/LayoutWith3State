1daf4d7465c10307dbd90068bdcb721413b9703e
amitabha-mycuratio <amitabha.biswas@mycuratio.com>
2015-09-15 12:54:13 +0530

    plotted demo value at gMap

--- SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
+++ SimpleSliderDemo/app/src/main/java/app/com/simplesliderdemo/SimpleSliderActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
@@ -13,7 +14,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,6 +34,8 @@ public class SimpleSliderActivity extends FragmentActivity
 {
     String jsonData = "{\"npidata\":[{\"id\":\"1831388404\",\"fullname\":\"AMERICAN CURRENT CARE PA\",\"type\":\"emergency\",\"latitude\":\"41.837642\",\"longitude\":\"-87.665741\",\"fulladdress\":\"3145 S ASHLAND AVE,SUITE 110,CHICAGO,IL,US,60608\",\"rating\":3,\"reviewCount\":9,\"primaryspecialty\":\"Urgent Care\",\"allspecialty\":\"Urgent Care,Clinic\\/Center\",\"distance\":2.0955347533003},{\"id\":\"1316388002\",\"fullname\":\"AMERICAN CURRENT CARE OF ILLINOIS PC\",\"type\":\"emergency\",\"latitude\":\"41.837642\",\"longitude\":\"-87.665741\",\"fulladdress\":\"3145 S ASHLAND AVE,SUITE 110,CHICAGO,IL,US,60608\",\"rating\":5,\"reviewCount\":44,\"primaryspecialty\":\"Urgent Care\",\"allspecialty\":\"Urgent Care\",\"distance\":2.0955347533003},{\"id\":\"1285823856\",\"fullname\":\"AMERICAN CURRENT CARE PA\",\"type\":\"emergency\",\"latitude\":\"41.827173\",\"longitude\":\"-87.729449\",\"fulladdress\":\"4201 WEST 36TH STREET,CHICAGO,IL,US,60632\",\"rating\":5,\"reviewCount\":49,\"primaryspecialty\":\"Urgent Care\",\"allspecialty\":\"Urgent Care\",\"distance\":2.4116189665392},{\"id\":\"1790030385\",\"fullname\":\"HOMETOWN URGENT CARE\",\"type\":\"emergency\",\"latitude\":\"41.850000\",\"longitude\":\"-87.650000\",\"fulladdress\":\"7483 SOLUTIONS CTR,CHICAGO,IL,US,60677\",\"rating\":2,\"reviewCount\":32,\"primaryspecialty\":\"Urgent Care\",\"allspecialty\":\"Urgent Care\",\"distance\":3.2576195954206},{\"id\":\"1649551938\",\"fullname\":\"OAK FOREST URGENT\\/IMMEDIATE CARE CLINIC\",\"type\":\"emergency\",\"latitude\":\"41.871516\",\"longitude\":\"-87.673764\",\"fulladdress\":\"1900 W POLK ST,SUITE 1348,CHICAGO,IL,US,60612\",\"rating\":5,\"reviewCount\":33,\"primaryspecialty\":\"Clinic\\/Center\",\"allspecialty\":\"Clinic\\/Center,Urgent Care\",\"distance\":4.1760342831924},{\"id\":\"1003084112\",\"fullname\":\"SOUTH LOOP URGENT CARE\",\"type\":\"emergency\",\"latitude\":\"41.861879\",\"longitude\":\"-87.624027\",\"fulladdress\":\"1430 S MICHIGAN AVE,C2,CHICAGO,IL,US,60605\",\"rating\":5,\"reviewCount\":2,\"primaryspecialty\":\"Urgent Care\",\"allspecialty\":\"Urgent Care\",\"distance\":4.7465122267332}],\"status\":1}";
     public String[] firstName, providerNpiID, lat, longg, address;
+    GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -52,10 +62,16 @@ public class SimpleSliderActivity extends FragmentActivity
         valueTV.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         linearLayout.addView(valueTV);
 
+        Double providerLatitude = 0.0, providerLongitude = 0.0;
+        BitmapDescriptor IconMarkerplot = BitmapDescriptorFactory.fromResource(R.drawable.ic_cast_light);
+        //map
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         try 
         {
             JSONObject jsonObject = new JSONObject(jsonData);
             JSONArray jsonArray = jsonObject.getJSONArray("npidata");
+            Log.d("jsonData", jsonData.replace("\"", ""));
+            Log.d("jsonArray", jsonArray.toString());
             firstName = new String[jsonArray.length()];
             providerNpiID = new String[jsonArray.length()];
             lat = new String[jsonArray.length()];
@@ -124,13 +140,26 @@ public class SimpleSliderActivity extends FragmentActivity
                 }
                 catch (Exception e)
                 {
-                    
+                    Log.d("CRASH 141 ", e.toString());
                 }
+
+
+
+                providerLatitude = Double.parseDouble(lat[i]);
+                providerLongitude = Double.parseDouble(longg[i]);
+                map.addMarker(new MarkerOptions()
+                        .position(new LatLng(providerLatitude, providerLongitude))
+//                                .title(String.valueOf(latLongHashMap.get("name" + loop))+" ")
+//                                .snippet(" "+providerAddress)
+                        .icon(IconMarkerplot));
             }
+
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(providerLatitude, providerLongitude), 12));
         }
         catch (Exception e)
         {
-            //
+            Log.d("CRASH 161 ", e.toString());
         }
     }
 }
